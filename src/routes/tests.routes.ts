b9000bc7d542673e7b7bb72e7d6444f8304ecbc5
Alisa Sosunova <alisasosunova@MacBook-Pro-Alisa.local>
2026-03-19 20:34:15 +0200

    add image API

--- src/routes/tests.routes.ts
+++ src/routes/tests.routes.ts
@@ -4,7 +4,7 @@ import { auth } from '../middleware/auth.middleware';
 import { adminOnly } from '../middleware/admin.middleware';
 import { TestCreateSchema } from '../validators/tests.schema';
 import { z } from 'zod';
-
+import mongoose from 'mongoose';
 
 const router = Router();
 
@@ -15,7 +15,7 @@ router.get('/all', async (req, res) => {
 
 router.get('/:id', auth, async (req, res) => {
     const id = req.params.id;
-    const test = await TestModel.find({_id: id});
+    const test = await TestModel.findById(id);
     res.json(test);
 });
 
@@ -26,9 +26,9 @@ router.post(
     async (req, res) => {
         try {
             const data = TestCreateSchema.parse(req.body)
-            const type = await TestModel.create(data)
+            const test = await TestModel.create(data)
 
-            res.status(201).json(type)
+            res.status(201).json(test)
         } catch (err) {
             if (err instanceof z.ZodError) {
                 return res.status(400).json({
@@ -42,5 +42,62 @@ router.post(
     }
 )
 
+router.put(
+    '/update/:id',
+    auth,
+    adminOnly,
+    async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const data = TestCreateSchema.parse(req.body);
+
+            const updatedTest = await TestModel.findByIdAndUpdate(
+                id,
+                data,
+                {
+                    new: true,
+                    runValidators: true
+                }
+            );
+
+            if (!updatedTest) {
+                return res.status(404).json({ message: 'Test not found' });
+            }
+
+            res.json(updatedTest);
+        } catch (err) {
+            if (err instanceof z.ZodError) {
+                return res.status(400).json({
+                    message: 'Validation error',
+                    errors: err.issues
+                });
+            }
+
+            res.status(500).json({ message: 'Update failed' });
+        }
+    }
+);
+
+router.delete(
+    '/delete/:id',
+    auth,
+    adminOnly,
+    async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const deletedTest = await TestModel.findByIdAndDelete(id);
+
+            if (!deletedTest) {
+                return res.status(404).json({ message: 'Test not found' });
+            }
+
+            res.json({ message: 'Test deleted successfully' });
+        } catch (err) {
+            res.status(500).json({ message: 'Delete failed' });
+        }
+    }
+);
 
 export default router;
